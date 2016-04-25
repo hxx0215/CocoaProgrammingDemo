cd5b4f9924962db66db9a9ba47c8d155f23db817
hxx <hxx0215@126.com>
2016-04-25 09:03:30 +0800

    RaiseMan

---
+++ RaiseMan/Employee.swift
@@ -0,0 +1,13 @@
+//
+//  Employee.swift
+//  RaiseMan
+//
+//  Created by ShadowFox on 4/24/16.
+//  Copyright © 2016 ShadowFox. All rights reserved.
+//
+
+import Foundation
+class Employee: NSObject {
+    var name: String? = "New Employee"
+    var raise: Float = 0.05
+}
---
+++ RaiseMan/RaiseMan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RaiseMan
+//
+//  Created by ShadowFox on 4/24/16.
+//  Copyright © 2016 ShadowFox. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
