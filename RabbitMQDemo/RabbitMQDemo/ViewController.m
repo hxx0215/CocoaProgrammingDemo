93806e3c666f3abec80a0bf8a7c0f571eb1dc6e1
hxx <hxx0215@126.com>
2016-06-25 22:31:37 +0800

    url

---
+++ RabbitMQDemo/RabbitMQDemo/ViewController.m
@@ -0,0 +1,61 @@
+//
+//  ViewController.m
+//  RabbitMQDemo
+//
+//  Created by ShadowFox on 6/17/16.
+//  Copyright © 2016 ShadowFox. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <RMQClient/RMQClient.h>
+#include <asl.h>
+
+//@import RMQClient;
+
+@interface ViewController ()
+@property(nonatomic, strong)id<RMQChannel> ch;
+@property(nonatomic, strong)RMQConnection *conn;
+@end
+
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [self send];
+    [self registerReceive];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)send{
+    self.conn = [[RMQConnection alloc] initWithUri:@"url" delegate:[RMQConnectionDelegateLogger new]];
+    [self.conn start];
+    
+    self.ch = [self.conn createChannel];
+}
+- (IBAction)sendMessage:(id)sender {
+    
+    RMQQueue *q = [self.ch queue:@"hello" options:RMQQueueDeclareDurable];
+    [q publish:@"hello world"];
+    asl_add_log_file(NULL, STDERR_FILENO);
+    asl_log(NULL,NULL,ASL_LEVEL_INFO,"Sent 'Hello World!'");
+    
+}
+
+- (void)registerReceive{
+    RMQConnection *conn = [[RMQConnection alloc] initWithUri:@"url" delegate:[RMQConnectionDelegateLogger new]];
+    [conn start];
+    
+    id<RMQChannel> ch = [conn createChannel];
+    
+    RMQQueue *q = [ch queue:@"hello" options:RMQQueueDeclareDurable];
+    asl_log(NULL,NULL,ASL_LEVEL_INFO,"Waiting for messages.");
+    [q subscribe:^(RMQMessage * _Nonnull message) {
+        NSLog(@"Received %@", message.content);
+    }];}
+@end
