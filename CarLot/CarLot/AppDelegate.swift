fe8025ccd70ac4910915129a20f11c7cb1329764
hxx <hxx0215@126.com>
2016-08-02 22:00:58 +0800

    delete demo

--- CarLot/CarLot/AppDelegate.swift
+++ CarLot/CarLot/AppDelegate.swift
@@ -12,27 +12,8 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 
-    func delay(seconds: NSTimeInterval, closure: ()->()) {
-        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(seconds * Double(NSEC_PER_SEC)))
-        dispatch_after(time, dispatch_get_main_queue()) {
-            print("🕑")
-            closure()
-        }
-    }
-    
-    func demo3() {
-        var value = 42
-        print("before closure: \(value)")
-        delay(1) { [copyVal = value]in
-            print("inside closure: \(copyVal)")
-        }
-        value = 1337
-        print("after closure: \(value)")
-    }
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
-        demo3()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
